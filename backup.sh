97648e4f2a3ab556fb52a1e12c7d4f993ce615d3
yujin-svr <yujin.gaya@gmail.com>
2017-02-21 05:52:23 +0900

    Use crontab to automatically backup

--- backup.sh
+++ backup.sh
@@ -1,1 +1,1 @@
-mysqldump --defaults-extra-file=/backup/script/account.cnf --all-databases > /backup/data/$(date +%Y%m%d).sql
+mysqldump --defaults-extra-file=/backup/script/account.cnf --all-databases > /backup/data/$(date +%Y%m%d).sql 2> /backup/log/err.log
